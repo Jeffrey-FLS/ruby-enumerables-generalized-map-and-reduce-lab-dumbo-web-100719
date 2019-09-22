55898cafcdd8921386ac3b54bc54027dc71d43cd
Jeffrey Paulino <jeffreypaulino@gmail.com>
2019-09-22 21:38:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,44 @@
-# Your Code Here
+
+def map(array)
+  new_array = []
+  i = 0
+
+  while i < array.length
+    # p "array value is #{array[i]}"
+    new_array << yield(array[i])
+    i += 1
+  end
+
+  # for i in array.length
+  #   p "array value is #{array[i]}"
+  #   new_array.push(yield(array[i]))
+  #   p new_array[i]
+  # end
+
+  return new_array
+end
+
+
+
+# def map(array)
+#   new_array = []
+
+# # p "array is #{array}"
+
+#   array.each do |i|
+#     new_array.push(yield(array[i]))
+#     p i
+#   end
+
+#   return new_array
+# end
+
+
+
+def reduce(array, val = nil)
+  
+  val == true : starting_value = val ? '' 
+  
+end
+
+
